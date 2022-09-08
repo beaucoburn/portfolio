cc220ae6b0bb9eefe420deab797c1357496a8814
Beau Coburn <56836072+beaucoburn@users.noreply.github.com>
2022-09-07 19:15:18 -0500

    Added Portfolio.js and beginning edits to Home

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          My name is Beau Coburn! Thank you for coming to visit me!
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="./porfolio/portfolio.js" className={styles.card}>
+            <h2>Portfolio &rarr;</h2>
+            <p>Please click here to see my portfolio.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>

26e364bfa65e272f5680cbee9b4936900ffb7ddb
Beau Coburn <douglas.coburn@gmail.com>
2023-02-16 11:37:52 -0600

    Commit 160223

--- pages/portfolio/QRCode.js
+++ pages/portfolio/QRCode.js
@@ -1,29 +1,31 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../components/qrcode.module.css";
+import { Outfit } from "@next/font/google";
 
-export default function QRCode({ Component, pageProps }) {
+const outfit = Outfit({ subsets: ['latin'] })
+
+export default function QRCode() {
     return (
-        <div className={styles.container} >
+        <div className={styles.container}>
           <h3>
             <Link href="/">Back to home</Link>
           </h3>
-          <div className={styles.qrcard} >
-            <Image 
-              height={576}
-              width={576}
-              src="/images/image-qr-code.png" 
+          <div className={styles.qrcard}>
+            <Image
+              height={250}
+              width={250}
+              src="/images/image-qr-code.png"
               className={styles.cardImgTop}
               alt="Frontend Mentor QR" />
             <div className={styles.cardBody}>
-              <h5 style={{ fontFamily:'Outfit', fontSize:'20px', weight:'700' }}>Improve your front-end skills by building projects</h5>
-              <p style={{ fontFamily:'Outfit', fontSize:'15px', weight:'400' }}>  Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+              <h5 className={outfit} style={{ weight: "700", fontSize: "20px" }}>Improve your front-end skills by building projects</h5>
+              <p className={outfit} style={{ weight: "400", fontSize: "15px" }}>  Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
             </div>
           </div>
-      
-          <br />
+         <br />
           <div className={styles.attribution}>
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
             Coded by <a href="https://www.frontendmentor.io/profile/beaucoburn">Beau Coburn</a>.
           </div>
         </div>
